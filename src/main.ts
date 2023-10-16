fa5f5eddae3e120079c7a0dfaef8efeeb1294358
alkavats1 <alka.vats@nashtechglobal.com>
2023-10-16 18:11:06 +0530

    notes-app-in-angular

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import 'zone.js/dist/zone';
+
+import { bootstrapApplication } from '@angular/platform-browser';
+
+import { AppComponent } from './app/app.component';
+
+
+bootstrapApplication(AppComponent)
+  .catch(err => console.error(err));
